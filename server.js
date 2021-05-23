853ddc918641b72514f6600f3e44f7a1ce72e747
disha03 <44141048+disha03@users.noreply.github.com>
2021-05-23 11:44:53 +0530

    server.js change

--- server.js
+++ server.js
@@ -14,16 +14,7 @@ connectDB();
 app.set('views',path.join(__dirname,'/views')); //for ejs
 app.set('view engine','ejs');
 
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/build'));
-  
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+
 //Routes
 app.use('/api/files', require('./routes/files'));
 app.use('/files',require('./routes/show'));  //for render to dowload page
